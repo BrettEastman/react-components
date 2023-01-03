3bbe35405235b6bf602daeb9731cfac960075434
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-01-03 12:02:39 -0800

    Refactor to GroceryList and GroceryListItem

--- app.js
+++ app.js
@@ -6,17 +6,40 @@ const Bagels = () => (
   <li>bagels</li>
 )
 
-const GroceryList = () => (
-  <ul>
-    <Coffee />
-    <Bagels />
-  </ul>
+// const GroceryList = () => (
+//   <ul>
+//     <Coffee />
+//     <Bagels />
+//   </ul>
+// )
+
+// const GroceryListItem = (props) => (
+//   <ul>
+//     <li>{props.groceryItems[0]}</li>
+//     <li>{props.groceryItems[1]}</li>
+//   </ul>
+// )
+const GroceryList = (props) => {
+  const onListItemClick = (event) => {
+    console.log('I got clicked');
+  };
+
+  return (
+    <ul>
+      <li onClick={onListItemClick}>{props.groceryItems[0]}</li>
+      <li>{props.groceryItems[1]}</li>
+    </ul>
+  );
+}
+
+const GroceryListItem = () => (
+  <GroceryList groceryItems={['coffee', 'bagels']}/>
 )
 
 const App = () => (
   <div>Some cliche salutation
     <h2>Grocery List</h2>
-    <GroceryList />
+    <GroceryListItem />
   </div>
 )
 
