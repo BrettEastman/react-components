c3ff73393ef7193a15144a80f75834d2e5ff9ddc
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-01-03 11:10:03 -0800

    Complete create GroceryList and item components

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// TODO
+const Coffee = () => (
+  <li>coffee</li>
+)
+
+const Bagels = () => (
+  <li>bagels</li>
+)
+
+const GroceryList = () => (
+  <ul>
+    <Coffee />
+    <Bagels />
+  </ul>
+)
+
+const App = () => (
+  <div>Some cliche salutation
+    <h2>Grocery List</h2>
+    <GroceryList />
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById("app"));
