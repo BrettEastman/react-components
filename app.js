41de61d9eac59f0363131e8741a5f2ca341a968e
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-01-03 13:24:48 -0800

    Refactor with destructured props

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const Coffee = () => (
-  <li>coffee</li>
-)
+// const Coffee = () => (
+//   <li>coffee</li>
+// )
 
-const Bagels = () => (
-  <li>bagels</li>
-)
+// const Bagels = () => (
+//   <li>bagels</li>
+// )
 
 // const GroceryList = () => (
 //   <ul>
@@ -19,27 +19,57 @@ const Bagels = () => (
 //     <li>{props.groceryItems[1]}</li>
 //   </ul>
 // )
-const GroceryList = (props) => {
-  const onListItemClick = (event) => {
-    console.log('I got clicked');
-  };
+
+///
+
+// const GroceryList = (props) => {
+//   const onListItemClick = (event) => {
+//     console.log('I got clicked');
+//   };
+
+//   return (
+//     <ul>
+//       <li onClick={onListItemClick}>{props.groceryItems[0]}</li>
+//       <li>{props.groceryItems[1]}</li>
+//     </ul>
+//   );
+// }
+
+// const GroceryListItem = () => (
+//   <GroceryList groceryItems={['coffee', 'bagels']}/>
+// )
+
+///
+
+// const props = (groceryItems: ['coffee', 'bagels'])
+
+// traditional "structured" way to write it:
+// const GroceryListItem = (props) => (
+//   <li>{props.groc}</li>
+// )
+
+// more common "destructured" way to write it
+const GroceryListItem = ({groc}) => (
+  <li>{groc}</li>
+)
+
+const GroceryList = () => {
+
+  let groceryItems = ['coffee', 'bagels'];
 
   return (
-    <ul>
-      <li onClick={onListItemClick}>{props.groceryItems[0]}</li>
-      <li>{props.groceryItems[1]}</li>
-    </ul>
-  );
-}
-
-const GroceryListItem = () => (
-  <GroceryList groceryItems={['coffee', 'bagels']}/>
-)
+  <ul>
+    {groceryItems.map((groc) => (
+      <GroceryListItem groc={groc} /> // setting up a props object, with key of groc. Could set up more properties. The first groc is the key, and the 2nd groc in curly braces is the variable
+    ))}
+  </ul>
+  )
+};
 
 const App = () => (
   <div>Some cliche salutation
     <h2>Grocery List</h2>
-    <GroceryListItem />
+    <GroceryList />
   </div>
 )
 
